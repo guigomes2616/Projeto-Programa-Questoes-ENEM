842150994c7c918fcf69cbba2cd6ef861bda73b9
Guilherme <guilherme.silva2616@etec.sp.gov.br>
2024-06-18 16:18:57 -0300

    attBancodedados18.06

--- 9-Config.php
+++ 9-Config.php
@@ -1,14 +1,10 @@
 <?php 
-/*
+
     $dbHost = '192.168.8.10';
     $dbUsername = 'grupophp06';
     $dbPassword = 'php06';
     $dbName = 'grupo06php';
-*/
-    $dbHost = 'localhost';
-    $dbUsername = 'root';
-    $dbPassword = '';
-    $dbName = 'Cadastro';  
+ 
     $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName );  
 
 ?>
